8b73ce4dd2c56db8f9ea660c8b1fa7633c61c372
pasiouras <pasiouras@windowslive.com>
2026-06-08 16:44:24 +0300

    Update paper bank

--- papers.js
+++ papers.js
@@ -32,16 +32,4 @@ const PAPER_BANK = `// === Mathematics_paper_1_SL.pdf [AA SL] ===
 // === Mathematics_paper_2_TZ2_SL.pdf [AA SL] ===
 
 
-// === Mathematics_paper_2_TZ2_SL_markscheme.pdf [AA SL — Mark Scheme] ===
-
-// === Mathematics_paper_1_SL.pdf [AA SL] ===
-
-
-// === Mathematics_paper_1_SL_markscheme.pdf [AA SL — Mark Scheme] ===
-
-
-// === Mathematics_paper_2_SL.pdf [AA SL] ===
-
-
-// === Mathematics_paper_2_SL_markscheme.pdf [AA SL — Mark Scheme] ===
-`;
+// === Mathematics_paper_2_TZ2_SL_markscheme.pdf [AA SL — Mark Scheme] ===`;
