5789660c257186639a5688598b6494f902679319
pasiouras <pasiouras@windowslive.com>
2026-06-08 17:57:03 +0300

    Add: Mathematics_paper_1__SL.pdf, Mathematics_paper_1__SL_markscheme.pdf

--- papers.js
+++ papers.js
@@ -32,4 +32,10 @@ const PAPER_BANK = `// === Mathematics_paper_1_SL.pdf [AA SL] ===
 // === Mathematics_paper_2_TZ2_SL.pdf [AA SL] ===
 
 
-// === Mathematics_paper_2_TZ2_SL_markscheme.pdf [AA SL — Mark Scheme] ===`;
+// === Mathematics_paper_2_TZ2_SL_markscheme.pdf [AA SL — Mark Scheme] ===
+
+// === Mathematics_paper_1__SL.pdf [AA SL] ===
+
+
+// === Mathematics_paper_1__SL_markscheme.pdf [AA SL — Mark Scheme] ===
+`;
